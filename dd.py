2c481d992ea5a1680a1893420f3e77dfe0d4ac24
park3hho <ejfhr7777@gmail.com>
2026-01-19 21:45:07 +0900

    # C Week 2 #
    =================================
    Semester: 2026.01.09 ~ 2026.04.17
    IDE: Pycharm
    Actual Env: Linux by Docker

---
+++ dd.py
@@ -0,0 +1,9 @@
+def solution(expressions):
+    eviden = []
+    result = []
+    for i in len(expressions):
+        if 'X' in i:
+            result.append(result)
+
+    answer = []
+    return answer
